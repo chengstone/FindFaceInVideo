2f5ce31a013efcec98c433f3d992df7879c68588
chengstone <69558140@163.com>
2017-09-02 18:12:11 +0800

    代码和数据上传

---
+++ VGGFace/IMG_3001/face_recognition.py
@@ -0,0 +1,232 @@
+import numpy as np
+import os
+import skimage
+import sys
+import caffe
+import sklearn.metrics.pairwise as pw
+import math
+
+#  sys.path.insert(0, '/Downloads/caffe-master/python');
+#  load Caffe model 
+caffe_root = '/home/chengstone/Downloads/caffe'
+sys.path.insert(0, caffe_root+'python')
+os.chdir(caffe_root)
+caffe.set_mode_cpu()
+#solver = caffe.SGDSolver('/home/chengshd/ML/caffe-master/examples/att_faces/attfaces_solver.prototxt')
+
+#caffe.set_mode_gpu();
+
+global net;
+net = caffe.Classifier('/home/chengstone/Downloads/caffe/VGGFace/VGG_FACE_deploy.prototxt', '/home/chengstone/Downloads/caffe/VGGFace/VGG_FACE.caffemodel');
+
+def compare_pic(feature1, feature2):
+    predicts = pw.cosine_similarity(feature1, feature2);
+    return predicts;
+
+def get_feature_new(path):
+    global net;
+    X = read_image_new(path);
+    # test_num = np.shape(X)[0];
+    # print test_num;
+    out = net.forward_all(data = X);
+    #print out
+    feature = np.float64(out['fc7']);
+    feature = np.reshape(feature, (1, 4096));
+    return feature;
+
+def read_image_other(filelist):
+
+    averageImg = [129.1863,104.7624,93.5940]
+    X=np.empty((1,3,224,224))
+    word=filelist.split('\n')
+    filename=word[0]
+    im1=skimage.io.imread(filename,as_grey=False)
+    #
+    image =skimage.transform.resize(im1,(224, 224))*255
+    X[0,0,:,:]=image[:,:,0]-averageImg[0]
+    X[0,1,:,:]=image[:,:,1]-averageImg[1]
+    X[0,2,:,:]=image[:,:,2]-averageImg[2]
+    return X
+
+def read_image_new(filepath):
+    averageImg = [129.1863, 104.7624, 93.5940];
+    X = np.empty((1,3,224,224));
+    filename = filepath.split('\n');
+    filename = filename[0];
+    im = skimage.io.imread(filename, as_grey=False);
+    image = skimage.transform.resize(im, (224, 224))*255;
+    #mean_blob.shape = (-1, 1); 
+    #mean = np.sum(mean_blob) / len(mean_blob);
+    X[0,0,:,:] = image[:,:,0] - averageImg[0];
+    X[0,1,:,:] = image[:,:,1] - averageImg[1];
+    X[0,2,:,:] = image[:,:,2] - averageImg[2];
+    return X;
+
+def get_feature(path, mean_blob):
+    global net;
+    X = read_image(path, mean_blob);
+    # test_num = np.shape(X)[0];
+    # print test_num;
+    out = net.forward_all(data = X);
+    feature = np.float64(out['fc256']);
+    feature = np.reshape(feature, (1, 256));
+    return feature;
+
+def read_image(filepath, mean_blob):
+    # averageImg = [129.1863, 104.7624, 93.5940];
+    X = np.empty((1,3,128,128));
+    filename = filepath.split('\n');
+    filename = filename[0];
+    im = skimage.io.imread(filename, as_grey=False);
+    image = skimage.transform.resize(im, (128, 128))*255;
+    mean_blob.shape = (-1, 1); 
+    mean = np.sum(mean_blob) / len(mean_blob);
+    X[0,0,:,:] = image[:,:] - mean;
+    X[0,1,:,:] = image[:,:] - mean;
+    X[0,2,:,:] = image[:,:] - mean;
+    return X;
+
+#'max accuracy: 0.894666666667', 
+#'max threshold: 0.769', 
+#'Max Precision: 0.919472913616 599', 
+#'Max Recall: 1.0 0', 
+#'Final F1Score: 0.224185221039', 
+#'Final Precision: 0.868980612883', 
+#'Final Recalls: 0.926333333333', 
+#'Best Accuracy: 0.893333333333', 
+#'Best Thershold: 0.753'
+
+def face_recog_test():
+    thershold = 0.85;
+    TEST_SUM = 15;
+    DATA_BASE = "/home/chengshd/ML/caffe-master/examples/VGGFace/";
+    MEAN_FILE = 'mean.binaryproto';
+    POSITIVE_TEST_FILE = "positive_pairs_path.txt";
+    NEGATIVE_TEST_FILE = "negative_pairs_path.txt";
+    
+    #mean_blob = caffe.proto.caffe_pb2.BlobProto();
+    #mean_blob.ParseFromString(open(MEAN_FILE, 'rb').read());
+    #mean_npy = caffe.io.blobproto_to_array(mean_blob);
+    
+    #print mean_npy.shape
+    thresholds = np.zeros(len(np.arange(0.4,1,0.05)))
+    Accuracys = np.zeros(len(np.arange(0.4,1,0.05)))
+    Precisions = np.zeros(len(np.arange(0.4,1,0.05)))
+    #AccuracyReals = np.zeros(len(np.arange(0.4,1,0.05)))
+    Recalls = np.zeros(len(np.arange(0.4,1,0.05)))
+    F1Score = np.zeros(len(np.arange(0.4,1,0.05)))
+    tick = 0
+    for thershold in np.arange(0.4, 1, 0.05):
+        True_Positive = 0;
+        True_Negative = 0;
+    	False_Positive = 0;
+    	False_Negative = 0;
+	print "==============================================="
+        # Positive Test
+        f_positive = open(DATA_BASE + POSITIVE_TEST_FILE, "r");
+        PositiveDataList = f_positive.readlines(); 
+        f_positive.close( );
+        #f_negative = open(NEGATIVE_TEST_FILE, "r");
+        #NegativeDataList = f_negative.readlines(); 
+        #f_negative.close( );
+        labels = np.zeros(len(PositiveDataList))
+        results = np.zeros(len(PositiveDataList))
+	thresholds[tick] = thershold
+        for index in range(len(PositiveDataList)):
+            filepath_1 = PositiveDataList[index].split(' ')[0];
+            filepath_2 = PositiveDataList[index].split(' ')[1];
+            labels[index] = PositiveDataList[index].split(' ')[2][:-2];
+            feature_1 = get_feature_new(DATA_BASE + filepath_1);
+            feature_2 = get_feature_new(DATA_BASE + filepath_2);
+            result = compare_pic(feature_1, feature_2);
+            #print "Two pictures similarity is:%f\n\n"%(result)
+            print "%s and %s Two pictures similarity is : %f\n\n"%(filepath_1,filepath_2,result)
+            #print "thershold: " + str(thershold);
+            if result>=thershold:
+                print 'Same person!!!!\n\n'
+            else:
+                print 'Different person!!!!\n\n'
+
+            if result >= thershold:
+                #  print 'Same Guy\n\n'
+                #True_Positive += 1;
+		results[index] = 1
+            else:
+                #  wrong
+                #False_Positive += 1;
+		results[index] = 0
+                
+	    if labels[index] == 1:
+		if results[index] == 1:
+			True_Positive += 1;
+		else:
+			False_Negative += 1;
+	    else:
+		if results[index] == 1:
+			False_Positive += 1;
+		else:
+			True_Negative += 1;
+        #for index in range(len(NegativeDataList)):
+        #    filepath_1 = NegativeDataList[index].split(' ')[0];
+        #    filepath_2 = NegativeDataList[index].split(' ')[1][:-2];
+        #    feature_1 = get_feature(DATA_BASE + filepath_1, mean_npy);
+        #    feature_2 = get_feature(DATA_BASE + filepath_2, mean_npy);
+        #    result = compare_pic(feature_1, feature_2);
+        #    if result >= thershold:
+        #        #  print 'Wrong Guy\n\n'
+        #        #  wrong
+        #        False_Negative += 1;
+        #    else:
+        #        #  correct
+        #        True_Negative += 1; 
+
+	if True_Positive + False_Positive == 0:
+		Precisions[tick] = 0
+	else:
+		Precisions[tick] = float(True_Positive) / (True_Positive + False_Positive)
+	#AccuracyReals[tick] = float(True_Positive + True_Negative) / (True_Positive + False_Positive + False_Negative + True_Negative)
+	if True_Positive + False_Negative == 0:
+		Recalls[tick] = 0
+	else:
+		Recalls[tick] = float(True_Positive) / (True_Positive + False_Negative)	
+
+	if Precisions[tick] + Recalls[tick] == 0:
+		F1Score[tick] = 0
+	else:
+		F1Score[tick] = (Precisions[tick] * Recalls[tick]) / (2 * (Precisions[tick] + Recalls[tick]))
+
+	acc = float(np.sum((labels == results))) / len(PositiveDataList)
+	print 'labels = ',labels
+	print 'results = ',results
+	Accuracys[tick] = acc
+	tick = tick + 1
+	print "Accuracy: " + str(float(acc));
+        
+        print "thershold: " + str(thershold);
+        #print "Accuracy: " + str(float(True_Positive + True_Negative)/TEST_SUM) + " %";
+        #print "True_Positive: " + str(float(True_Positive)/TEST_SUM) + " %";
+        #print "True_Negative: " + str(float(True_Negative)/TEST_SUM) + " %";
+        #print "False_Positive: " + str(float(False_Positive)/TEST_SUM) + " %";
+        #print "False_Negative: " + str(float(False_Negative)/TEST_SUM) + " %";
+    print 'Accuracys: ', Accuracys
+    print 'Thresholds: ', thresholds
+    print 'Precisions: ', Precisions
+    print 'Recalls: ', Recalls
+    print 'F1Score: ', F1Score
+    #print 'AccuracyReals: ',AccuracyReals
+
+    print 'Max Precision: ', np.max(Precisions), np.where(Precisions == np.max(Precisions))[0][0]
+    print 'Max Recall: ', np.max(Recalls), np.where(Recalls == np.max(Recalls))[0][0]
+
+    print "Final Accuracy: ", np.max(Accuracys)
+    re = np.where(Accuracys == np.max(Accuracys))
+    print 'Final Thershold: ', thresholds[re[0][0]]
+    print 'Final F1Score: ', np.max(F1Score)
+    re = np.where(F1Score == np.max(F1Score))
+    print 'Final Precision: ',Precisions[re[0][0]]
+    print 'Final Recalls: ',Recalls[re[0][0]]
+    print 'Best Accuracy: ',Accuracys[re[0][0]]
+    print 'Best Thershold: ',thresholds[re[0][0]]
+
+if __name__ == '__main__':
+    face_recog_test()
---
+++ VGGFace/facedetect.py
@@ -0,0 +1,571 @@
+#!/usr/bin/env python
+
+#
+# __Author__ chengstone
+# __WeChat__ 15041746064
+# __e-Mail__ 69558140@163.com
+# 
+
+import numpy as np
+import cv2
+import cv2.cv as cv
+from matplotlib import pyplot as plt
+#from video import create_capture
+# from common import clock, draw_str
+import clock
+import subprocess
+import os
+import sys, getopt
+import threading
+import signal
+import shutil
+from argparse import ArgumentParser
+
+from face_recognition import *
+from lfw_test_deal import *
+
+help_message = '''
+USAGE: facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
+
+DEBUG_FLAG = 1
+
+VIDEO_FLAG = 0
+
+MAIN_PATH = '/home/chengstone/Downloads/caffe/VGGFace/'
+CAS_PATH = '/home/chengstone/Downloads/opencv-2.4.13.2/data/haarcascades/'
+FRONTAL_FACE = 'haarcascade_frontalface_default.xml'
+
+# IMAGE_FILE_PATH = '/home/chengshd/ML/caffe-master/examples/VGGFace/images/'
+
+TargetPath = MAIN_PATH + 'targets.txt'
+camPicture = '/home/chengshd/ML/caffe-master/examples/VGGFace/chengshd/IMG_3167.JPG'#MAIN_PATH + 'IMG_0468.JPG'
+
+VIDEO_HOME = '/home/chengshd/ML/caffe-master/examples/VGGFace/chengshd/'
+videoPath = VIDEO_HOME + 'IMG_3170.mp4'    #'IMG_3663.MOV'
+#test_obj = None
+
+targetsArr = []
+dstArr = []
+FoundFace = 0
+
+FaceCropPath = '/home/chengstone/Downloads/SeetaFaceEngine/FaceAlignment/build/'
+FA_TEST = './fa_test'
+IMAGE_TXT = 'image.txt'
+
+del_threshold = 0.15    #0.25
+last_scaleFactor = 1.0655
+
+cascade_fn = CAS_PATH + FRONTAL_FACE#args.get('--cascade', "../../data/haarcascades/haarcascade_frontalface_alt.xml")
+cascade = cv2.CascadeClassifier(cascade_fn)
+
+g_vis = None
+dst_rects = None
+VGG_THRESHOLD = 0.4    #0.753
+
+createFaceFlag = 0
+
+# cascv_fn = CAS_PATH + 'haarcascade_frontalface_alt.xml'
+# cascade_cv = cv2.CascadeClassifier('/home/chengshd/ML/opencv-2.4.13.2/data/haarcascades/haarcascade_frontalface_alt.xml')
+
+
+def detect(img, cascade):
+    # print 'detect IN:'
+    rects = cascade.detectMultiScale(img, scaleFactor=1.06559, minNeighbors=4, minSize=(5, 5), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+def draw_rects(img, rects, color):
+    # print 'draw_rects IN:'
+    print rects
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+def isOverlap(tmp_rect, rects):
+    i = 0
+    for x1, y1, x2, y2 in rects:
+        if(tmp_rect[0] != x1 and tmp_rect[1] != y1 and tmp_rect[2] != x2 and tmp_rect[3] != y2):
+            if(tmp_rect[2] > x1 and x2 > tmp_rect[0] and tmp_rect[3] > y1 and y2 > tmp_rect[1]):
+                return i
+        i = i + 1
+    return -1
+
+def computeRectJoinUnion(rect1, rect2):
+    x1 = max(rect1[0], rect2[0])
+    y1 = max(rect1[1], rect2[1])
+
+    x2 = min(rect1[2], rect2[2])
+    y2 = min(rect1[3], rect2[3])
+
+    AJoin = 0
+    if(x2 > x1 and y2 > y1):
+        AJoin = (x2 - x1) * (y2 - y1)
+
+    A1 = (rect1[2] - rect1[0]) * (rect1[3] - rect1[1])
+    A2 = (rect2[2] - rect2[0]) * (rect2[3] - rect2[1])
+
+    AUnion = A1 + A2 - AJoin
+
+    if(AUnion > 0):
+        return float(AJoin) / AUnion
+    return 0
+
+def procOverlap(rects):
+    print 'procOverlap IN:'
+    #print rects
+    i = 0
+    new_rects = []
+    del_rects = []
+    for x1, y1, x2, y2 in rects:
+        tmp_rect = rects[i]
+        bOverlap = isOverlap(tmp_rect, rects)
+        if(bOverlap > -1):
+            print tmp_rect
+            print rects[bOverlap], bOverlap
+            rectJountUnion = computeRectJoinUnion(tmp_rect, rects[bOverlap])
+            print 'rectJountUnion = ', rectJountUnion
+            if(rectJountUnion > del_threshold):
+                A1 = (tmp_rect[2] - tmp_rect[0]) * (tmp_rect[3] - tmp_rect[1])
+                A2 = (rects[bOverlap][2] - rects[bOverlap][0]) * (rects[bOverlap][3] - rects[bOverlap][1])
+                if(A1 < A2):
+                    new_rects.append(tmp_rect)
+                    #np.delete(rects, bOverlap, 0)
+                    del_rects.append(rects[bOverlap])
+                    print 'A1 < A2'
+                #else:
+            else:
+                new_rects.append(tmp_rect)
+        else:
+            new_rects.append(tmp_rect)
+        #print 'i = ', i
+        #print 'new_rects = ', new_rects
+        #print new_rects[i], i
+        i = i + 1
+    
+    del_idx = []
+    for node in del_rects:
+        j = 0
+        for new_node in new_rects:
+            #print new_node[0],new_node[1],new_node[2],new_node[3]
+            if(node[0] == new_node[0] and node[1] == new_node[1] and node[2] == new_node[2] and node[3] == new_node[3]):
+                del_idx.append(j)
+            #if(node == new_node):
+            #    print node,' is equel.'
+            j = j + 1
+    print del_idx
+
+    for idx in del_idx:
+        del new_rects[idx]
+
+    #print new_rects
+    print 'procOverlap done.'
+    return np.array(new_rects)
+
+def ProcImage(imagePath, modifyPath):
+    print 'ProcImage IN:'
+    
+    img = cv2.imread(imagePath)
+    print imagePath
+    sp = img.shape
+    while(sp[0] > 768 + 512 or sp[1] > 1024 + 1024):    #sp[0]: height sp[1]: width sp[3]: tongdao
+        img = cv2.pyrDown(img)
+        sp = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    # t = clock().utc()
+    rects = detect(gray, cascade)
+    vis = img.copy()
+    print rects
+    		#print rects.shape#, rects.dtype, rects
+    new_rects = procOverlap(rects)
+    print new_rects.shape
+    
+    if DEBUG_FLAG == 1:
+    	draw_rects(vis, new_rects, (0, 255, 0))
+    
+    
+    if(modifyPath[-1] != '/'):
+        modifyPath = modifyPath + '/'
+    cv2.imwrite(modifyPath + imagePath.split('/')[-1].split('.')[0] + '_modify.' + imagePath.split('/')[-1].split('.')[-1], vis)
+    
+def facedetect_test():
+    #file_name = 'IMG_0468.JPG'
+    #file_name = '2.jpg'
+    #file_names = ['81.jpg','84.jpg','92.jpg','94.jpg','104.jpg','116.jpg','194.jpg','203.jpg','1.jpg','2.jpg','IMG_0468.JPG']
+    #file_names = ['81.jpg','84.jpg','104.jpg','IMG_0468.JPG']
+    file_names = ['qingyansi.jpg']    
+
+    #for parent, dirnames, filenames in os.walk(IMAGE_FILE_PATH):
+    #    #print parent,dirnames,filenames
+    #	for file_name in filenames:
+    #    	imagePath = parent + file_name
+    for file_name in file_names:
+    	imagePath = MAIN_PATH + file_name
+        ProcImage(imagePath, MAIN_PATH)
+
+def checkFile(filepath):
+    path = ''
+    for field in filepath.split('/'):
+        if len(field) > 0:
+            path = path + '/' + field
+            #print path, os.path.exists(path)
+            if field == filepath.split('/')[-1]:
+                # print path, path.find('.')
+                if path.find('.') != -1:
+                    if os.path.exists(path) == False:
+                        os.mknod(path)
+                elif os.path.exists(path) == False:
+                    # print path
+                    os.mkdir(path)
+            elif os.path.exists(path) == False:
+                os.mkdir(path)
+
+def returnFaceImg_Dst_2(imagePath):
+    # print 'returnFaceImg_Dst IN:'
+
+    global FoundFace
+    global dst_rects
+    global g_vis
+
+    dstpath = MAIN_PATH + "output/"
+    corp_size = 224
+
+    imagetxt_file = open(FaceCropPath + IMAGE_TXT, 'w')
+    imagetxt_file.writelines(imagePath + ' ' + dstpath + ' ' + str(corp_size) + '\n')
+    imagetxt_file.close()
+
+    checkFile(dstpath)
+
+    if createFaceFlag == 1:
+        os.chdir(FaceCropPath)
+        os.system(FA_TEST)
+    # cv2.waitKey(500)
+    # print 'here'
+
+    FoundFace = 0
+    new_rects = []
+    t_imgsArr = []
+    for parent, dirnames, filenames in os.walk(dstpath):
+        for f_file in filenames:
+            # if "result" in filenames:
+            if f_file.find("result") == -1:
+                print parent, dirnames, f_file
+                t_imgsArr.append(get_feature_new(parent + f_file))
+                #qingyansi_crop_224_0_333_715_425_807.jpg
+                X1 = int(f_file.split('.')[0].split('_')[-4])
+                Y1 = int(f_file.split('.')[0].split('_')[-3])
+                X2 = int(f_file.split('.')[0].split('_')[-2])
+                Y2 = int(f_file.split('.')[0].split('_')[-1])
+                new_rects.append([X1, Y1, X2, Y2])
+                FoundFace = 1
+            # else:
+            #     img = cv2.imread(parent + f_file)
+            #     plt.subplot(1, 2, 1)
+            #     plt.imshow(img)
+                # plt.show()
+                # cv2.namedWindow(f_file)
+                # cv2.imshow(f_file, img)
+                # cv2.waitKey(5)
+    dst_rects = np.array(new_rects)
+
+    img = cv2.imread(imagePath)
+    print 'img.dtype = ', img.dtype
+    sp = img.shape
+    while (sp[0] > 768 + 512 or sp[1] > 1024 + 1024):  # sp[0]: height sp[1]: width sp[3]: tongdao
+        img = cv2.pyrDown(img)
+        sp = img.shape
+
+    vis = img.copy()
+    g_vis = vis
+
+    if DEBUG_FLAG == 1:
+        draw_rects(vis, dst_rects, (255, 0, 0))
+
+    print np.array(t_imgsArr).shape
+    return np.array(t_imgsArr)
+
+
+def returnFaceImg_Dst(imagePath):
+    # print 'returnFaceImg_Dst IN:'
+
+    global FoundFace
+    global dst_rects
+    global g_vis
+    img = cv2.imread(imagePath)
+    print 'img.dtype = ', img.dtype
+    #aa = cv.CloneMat(np.fromarrays(img))
+    #print aa.dtype
+
+    # cv.CreateMat(img)
+
+
+    #print imagePath
+    sp = img.shape
+    while(sp[0] > 768 + 512 or sp[1] > 1024 + 1024):    #sp[0]: height sp[1]: width sp[3]: tongdao
+        img = cv2.pyrDown(img)
+        sp = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    rects = detect(gray, cascade)
+    vis = img.copy()
+    g_vis = vis
+
+    print 'dst rects =',rects,vis.dtype,vis.shape
+    if len(rects) != 0:
+        print imagePath + ' Face found'
+        FoundFace = 1
+        new_rects = procOverlap(rects)
+        print new_rects.shape
+
+        t_dstsArr = []
+        for rect in new_rects:
+            print rect[0], rect[1], rect[2]-rect[0], rect[3]-rect[1]
+            
+            print imagePath.split('/')[-2]
+            vis2 = vis[rect[1]:rect[3], rect[0]:rect[2], :]
+            # vis2 = vis[rect[2]:rect[0], rect[3]:rect[1], :]
+            cv2.imwrite("./tmp.jpg", vis2)
+            # aaa = cv2.imread("./tmp.jpg")
+            # plt.imshow(aaa)
+            # plt.show()
+            t_dstsArr.append(get_feature_new("./tmp.jpg"))
+
+        if DEBUG_FLAG == 1:
+            draw_rects(vis, new_rects, (255, 0, 0))
+            # plt.imshow(vis)
+            # plt.show()
+
+        dst_rects = new_rects
+        print np.array(t_dstsArr).shape
+        return np.array(t_dstsArr)
+
+    else:
+        FoundFace = 0
+        print imagePath + ' Face not found'
+        return []
+
+#/home/chengshd/ML/caffe-master/examples/att_faces/s40/2.jpg /home/chengshd/ML/caffe-master/examples/att_faces_new/s40 224
+#fullpath dstpath cropSize
+def returnFaceImg(imagePath, dstpath, corp_size):
+    # print 'returnFaceImg IN:'
+    imagetxt_file = open(FaceCropPath + IMAGE_TXT, 'w')
+    imagetxt_file.writelines(imagePath + ' ' + dstpath + ' ' + str(corp_size) + '\n')
+    imagetxt_file.close()
+
+    checkFile(dstpath)
+
+    if createFaceFlag == 1:
+        os.chdir(FaceCropPath)
+        os.system(FA_TEST)
+    #cv2.waitKey(500)
+    #print 'here'
+    
+    t_targetsArr = []
+    for parent, dirnames, filenames in os.walk(dstpath):
+        for f_file in filenames:
+            #if "result" in filenames:
+            if f_file.find("result") == -1:
+                print parent, dirnames, f_file
+                t_targetsArr.append(get_feature_new(parent + f_file))
+            else:
+                img = cv2.imread(parent + f_file)
+                plt.subplot(1, 2, 1)
+                b, g, r = cv2.split(img)
+                img2 = cv2.merge([r, g, b])
+                if VIDEO_FLAG != 1:
+                    plt.imshow(img2)
+                # plt.show()
+                #cv2.namedWindow(f_file)
+                #cv2.imshow(f_file, img)
+                #cv2.waitKey(5)
+
+    print np.array(t_targetsArr).shape
+    return np.array(t_targetsArr)
+
+def getTargetFace():
+    # print 'getTargetFace IN:'
+    global targetsArr
+    if os.path.exists(TargetPath) == False:
+        print TargetPath + ' File not found'
+        exit(0)
+
+    fileObj = open(TargetPath)
+    fileObjDataList = fileObj.readlines(); 
+    fileObj.close()
+
+    #for line in fileObj:
+    #    print line
+    for index in range(len(fileObjDataList)):    
+        line = fileObjDataList[index]
+        print index, line[:-1]
+        if os.path.exists(line[:-1]) == False:
+            print TargetPath + ' File not found'
+        else:
+            targetsArr.append(returnFaceImg(line[:-1], MAIN_PATH + line[:-1].split('/')[-1].split('.')[0] + '/', 224))
+
+def getDstFace_2():
+    # print 'getDstFace IN:'
+    global dstArr
+    dstArr.append(returnFaceImg_Dst_2(camPicture))
+    # print dstArr
+
+def getDstFace():
+    # print 'getDstFace IN:'
+    global dstArr
+    dstArr.append(returnFaceImg_Dst(camPicture))
+    # print dstArr
+
+def draw_single_rect(img, rect, color):
+    # print 'draw_single_rect IN:'
+    # for x1, y1, x2, y2 in rect:
+    cv2.rectangle(img, (rect[0], rect[1]), (rect[2], rect[3]), color, 2)
+
+def prediction():
+    print 'prediction IN:'
+    global targetsArr
+    global dstArr
+    global dst_rects
+
+    # t_dstArr = np.array(dstArr)
+    # print dstArr
+    # try:
+    if 1 == 1:
+        if FoundFace == 1:
+        # if len(dstArr[0]) != 0:
+            dstShape = dstArr[0].shape
+            print 'dst shape: ', dstShape[0]
+
+            targetShape = targetsArr[0].shape
+            print 'target shape: ', targetShape[0]
+
+            results = np.zeros([dstShape[0], targetShape[0]])
+            print '===========predict results: ==========='
+            for i in range(dstShape[0]):
+                for j in range(targetShape[0]):
+                    # print results[i][j]
+                    results[i][j] = compare_pic(dstArr[0][i], targetsArr[0][j])
+                    print results[i][j]
+
+            # print dstArr[0][0].shape, dstArr[0][0]
+            # result = compare_pic(feature_1, feature_2);
+            for i in range(dstShape[0]):
+                for j in range(targetShape[0]):
+                    if results[i][j] >= VGG_THRESHOLD:
+                        draw_single_rect(g_vis, dst_rects[i], (0, 255, 0))
+                    if DEBUG_FLAG == 1:
+                        if results[i][j] >= VGG_THRESHOLD:
+                            pen = (0, 255, 0)
+                        else:
+                            pen = (255, 0, 0)
+                        cv2.putText(g_vis, str(round(results[i][j], 2)), (dst_rects[i][0], dst_rects[i][1] - 7), cv2.FONT_HERSHEY_DUPLEX, 0.8, pen)
+            cv2.imwrite(MAIN_PATH + "tmp.jpg", g_vis)
+            plt.subplot(1, 2, 2)
+            b, g, r = cv2.split(g_vis)
+            img2 = cv2.merge([r, g, b])
+            # plt.figure(num=1)
+            plt.imshow(img2)
+            if VIDEO_FLAG != 1:
+                plt.show()
+    
+def findPersionByImage_2():
+    global targetsArr
+    global dstArr
+    getTargetFace()
+    #print targetsArr[0].shape
+
+    getDstFace_2()
+    # print dstArr[0].shape
+
+    prediction()
+    print 'done'
+
+
+def findPersionByImage():
+    global targetsArr
+    global dstArr
+    getTargetFace()
+    #print targetsArr[0].shape
+
+    getDstFace()
+    # print dstArr[0].shape
+
+    prediction()
+    print 'done'
+
+def findPersionByVideo():
+    global targetsArr
+    global dstArr
+    global camPicture
+    getTargetFace()
+
+    videoCapture = cv2.VideoCapture(videoPath)
+    fps = videoCapture.get(cv2.cv.CV_CAP_PROP_FPS)
+    size = (int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+    framesCount = videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+    # videoWriter = cv2.VideoWriter(MAIN_PATH + 'out/' + videoPath.split('/')[-1], cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), fps, size)
+    videoWriter = cv2.VideoWriter(MAIN_PATH + 'out/test.avi', cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'),
+                                  fps, size)
+    print 'videoWrite = ' + MAIN_PATH + 'out/test.avi'
+
+    success, frame = videoCapture.read()
+
+    print 'frame.shape = ', frame.shape
+    tick = 0
+    while success:    # and tick < 10
+        tick = tick + 1
+        print 'Current frame : ' + str(tick) + ' / ' + str(framesCount)
+
+        dstArr = []
+        
+        cv2.imwrite(MAIN_PATH + "frame_tmp.jpg", frame)
+        camPicture = MAIN_PATH + "frame_tmp.jpg"
+        getDstFace()
+        # getDstFace_2()
+        prediction()
+
+        print 'g_vis.shape = ', g_vis.shape
+        cv2.imshow("find persion by video", g_vis)
+        # cv2.imshow("find persion by video", frame)
+        cv2.waitKey(1000/int(fps))
+        videoWriter.write(g_vis)
+        # videoWriter.write(frame)
+        success, frame = videoCapture.read()
+    videoCapture.release()
+    videoWriter.release()
+    
+    print 'findPersionByVideo done'
+
+if __name__ == '__main__':
+    global VIDEO_FLAG
+    parser = ArgumentParser()  
+      
+    parser.add_argument('--content',  
+            dest='content', help='image/video path',  
+            metavar='CONTENT', required=True)  #
+    
+    options = parser.parse_args()  
+      
+    content = options.content # the image/video name you want to detect  
+
+    if content.split('.')[-1] in ['jpg', 'JPG', 'jpeg', 'bmp', 'png']:
+            print content
+            camPicture = content
+            findPersionByImage()
+    elif content.split('.')[-1] in \
+        ['mov', 'MOV', 'rm', 'RM', \
+         'rmvb', 'RMVB', 'mp4', 'MP4', \
+         'avi', 'AVI', 'wmv', 'WMV', \
+         '3gp', '3GP', 'mpeg', 'MPEG', \
+         'mkv', 'MKV']:
+        print content
+        VIDEO_FLAG = 1
+        videoPath = content
+        findPersionByVideo()
+    #findPersionByImage()
+    # findPersionByVideo()
+    # findPersionByImage_2()
+
